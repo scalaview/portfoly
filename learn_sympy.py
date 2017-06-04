8e3794f8d90e7bf5073a129f56a1bd8b3685cac6
scalaview <chailink100@gmail.com>
2017-06-04 19:11:45 +0800

    add lean

--- learn_sympy.py
+++ learn_sympy.py
@@ -244,9 +244,9 @@ def srd_euler():
   xh[0] = x0
   x1[0] = x0
   for t in range(1, M+1):
-    xh[t] = (xh[t-1] + kappa * (theta - np.maximun(xh[t-1], 0)) * dt \
-    + sigma * np.sqrt(np.maximun(xh[t-1], 0)) * np.sqrt(dt) * npr.standard_normal(I))
-  x1 = np.maximun(xh, 0)
+    xh[t] = (xh[t-1] + kappa * (theta - np.maximum(xh[t-1], 0)) * dt \
+    + sigma * np.sqrt(np.maximum(xh[t-1], 0)) * np.sqrt(dt) * npr.standard_normal(I))
+  x1 = np.maximum(xh, 0)
   return x1
 x1 = srd_euler()
 
@@ -257,6 +257,53 @@ plt.show()
 plt.plot(x1[:, :10], lw=1.5)
 plt.grid(True)
 plt.show()
+
+
+
+S0 = 100
+r = 0.05
+v0 = 0.1
+kappa = 3.0
+theta = 0.25
+sigma = 0.1
+rho = 0.6
+T = 1.0
+
+
+corr_mat = np.zeros((2, 2))
+corr_mat[0, :] = [1.0, rho]
+corr_mat[1, :] = [rho, 1.0]
+cho_mat = np.linalg.cholesky(corr_mat)
+
+M = 50
+I = 10000
+ran_num = npr.standard_normal((2, M + 1, I))
+
+dt = T / M
+v = np.zeros_like(ran_num[0])
+vh = np.zeros_like(v)
+v[0] = v0
+vh[0] = v0
+for t in range(1, M+1):
+  ran = np.dot(cho_mat, ran_num[:, t, :])
+  vh[t] = (vh[t-1] + kappa * (theta - np.maximum(vh[t-1], 0)) * dt + sigma * np.sqrt(np.maximum(vh[t-1], 0)) * np.sqrt(dt) + ran[1])
+
+v = np.maximum(vh, 0)
+
+S = np.zeros_like(ran_num[0])
+S[0] = S0
+for t in range(1, M+1):
+  ran = np.dot(cho_mat, ran_num[:, t, :])
+  S[t] = S[t-1] * np.exp((r-0.5 * v[t]) * dt + np.sqrt(v[t]) * ran[0] * np.sqrt(dt))
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 5))
+ax1.hist(S[-1], bins=50)
+ax1.grid(True)
+
+ax2.hist(v[-1], bins=50)
+ax2.grid(True)
+
 
 
 
